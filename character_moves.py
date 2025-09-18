417588524d654644df27fb6060fb5c6285503136
FakeZero <sjlimdaa@naver.com>
2025-09-19 01:20:25 +0900

    사각운동, 원운동 뼈대 완성

--- character_moves.py
+++ character_moves.py
@@ -1,6 +1,15 @@
 from pico2d import *
 import math
 
+
+def square_move():
+    pass
+
+
+def circle_move():
+    pass
+
+
 if __name__ == '__main__':
     open_canvas()
 
@@ -8,6 +17,8 @@ if __name__ == '__main__':
     character = load_image('character.png')
 
     while True:
+        square_move()
+        circle_move()
         pass
 
     close_canvas()
