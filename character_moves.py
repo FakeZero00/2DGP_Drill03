dd1e2e09f82ecad5432601a1fddb984706bf4b9b
FakeZero <sjlimdaa@naver.com>
2025-09-19 03:07:21 +0900

    circle_move 함수 완성

--- character_moves.py
+++ character_moves.py
@@ -33,8 +33,19 @@ def square_move(character, wallpaper):
     move_direction(character, wallpaper, 'down', 470)
     move_direction(character, wallpaper, 'right', 380)
 
-def circle_move():
-    pass
+def circle_move(character, wallpaper, origin, radius):
+    global char_x
+    global char_y
+
+    for deg in range(270, -90, -1):
+        clear_canvas_now()
+        wallpaper.draw_now(400, 30)
+
+        char_x = origin[0] + radius * math.cos(math.radians(deg))
+        char_y = origin[1] + radius * math.sin(math.radians(deg))
+
+        character.draw_now(char_x, char_y)
+        delay(0.001)
 
 
 if __name__ == '__main__':
@@ -46,7 +57,7 @@ if __name__ == '__main__':
     while True: #배경 그리기
 
         square_move(character, grass)
-        #circle_move()
+        circle_move(character, grass, (400, 300), 210)
         pass
 
     close_canvas()
